73488325da0af6c85ab0732061bfd9e9fda76b54
jdlee <jdlee@bmcnet.kr>
2024-11-22 11:56:13 +0900

    로그인 유저 이름 노출

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Cookies from 'js-cookie';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 
 export default function Header() {
     const router = useRouter();
+    const [userName, setUserName] = useState("");
 
     //로그인 여부 확인
     async function loginConfirm() {
@@ -12,6 +13,7 @@ export default function Header() {
         if(currentUuid) {
             const response = await fetch(`http://scwc2024.cafe24.com/controller/admin/manager/adminInfo.php?managerUuid=${currentUuid}`);
             const loginUserInfo = await response.json();
+            setUserName(loginUserInfo?.list[0].name);
             if(loginUserInfo?.list[0].uuid !== currentUuid) {
                 router.push('/')
                 Cookies.remove('hessid')
@@ -36,7 +38,7 @@ export default function Header() {
         <header>
             <h1 className="logo"><Image src="/images/ichoen-facility-corporation-white-logo.svg" alt="인천시설공단" width={197} height={46}/></h1>
             <p className="page-title">인천노인복지회관 - 화재감지 시스템</p>
-            <div className="logout">홍길동 | <a href="#" onClick={Logout}>로그아웃</a></div>
+            <div className="logout">{userName} | <a href="#" onClick={Logout}>로그아웃</a></div>
         </header>
     )
 }
