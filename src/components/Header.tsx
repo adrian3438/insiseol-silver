cdd6e70c82e1dad4ed97b547ef2e60a3ba59273a
jdlee <jdlee@bmcnet.kr>
2025-12-05 14:33:34 +0900

    로그인 페이지 텍스트 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     const [userName, setUserName] = useState("");
 
     //로그인 여부 확인
-    async function loginConfirm() {
+    /*async function loginConfirm() {
         const currentUuid = Cookies.get('hessid');
         if(currentUuid) {
             const response = await fetch(`http://scwc2024.cafe24.com/controller/admin/manager/adminInfo.php?managerUuid=${currentUuid}`);
@@ -25,7 +25,7 @@ export default function Header() {
     }
     useEffect(() => {
         loginConfirm();
-    }, []);
+    }, []);*/
 
 
     //로그아웃
@@ -38,7 +38,7 @@ export default function Header() {
         <header>
             <h1 className="logo"><Image src="/images/ichoen-facility-corporation-white-logo.svg" alt="인천시설공단" width={197} height={46}/></h1>
             <p className="page-title">인천노인복지회관 - 화재감지 시스템</p>
-            <div className="logout">{userName} | <a href="#" onClick={Logout}>로그아웃</a></div>
+            <div className="logout">{userName} <a href="#" onClick={Logout}>로그아웃</a></div>
         </header>
     )
 }
